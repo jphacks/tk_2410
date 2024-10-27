6004821063722ed11294ddd4f4081dc3e387b484
yatoyun <kaneko.yuta0813@gmail.com>
2024-10-27 12:21:53 +0900

    front api clientを実装

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
 import reactLogo from "./assets/react.svg";
+import { anonymousLogin } from "./api";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const handleLogin = async () => {
+    // FIXME
+    // デモ
+    const deviceId = '8f0315e3-33a4-42cb-a527-39531a7c0e9f';
+    await anonymousLogin(deviceId);
+  };
 
   return (
     <>
@@ -21,6 +29,7 @@ function App() {
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={handleLogin}>Login Anonymously</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
